243518c3c0923dff03022ccf41c39302cbfdc85e
= <=>
2016-08-11 17:50:39 +0100

    Change example to use build and remove logs

--- examples/bnf/index.js
+++ examples/bnf/index.js
@@ -11,32 +11,7 @@ g.addRules({
     'OptionalWhitespace' : [['WS',"OptionalWhitespace"],['']]
 });
 
-/*g.build('S');
+var start = g.build('Start');
 
-Diagram.drawStateDiagram(g.stateOptions.S[0],
-    document.getElementById('container'));
-*/
-
-Object.keys(g.rules).forEach(function(ruleLabel){
-    this.firstPass(this.rules[ruleLabel],ruleLabel);
-}, g);
-
-function buildPreRules(sOps) {
-    var nodes = [], edges = [], s, data;
-    Object.keys(sOps).forEach(function(ruleLabel) {
-        s = new State("", ruleLabel);
-        s.addStates(sOps[ruleLabel]);
-        console.log(s);
-        data = Diagram.buildData(s);
-        nodes = nodes.concat(data.nodes);
-        edges = edges.concat(data.edges);
-    });
-    Diagram.drawFromData(nodes,edges,document.getElementById("container2"));
-};
-
-buildPreRules(g.stateOptions);
-
-var start = new State("","Start");
-start.addStates(g.buildTraverse("Start"));
 Diagram.drawStateDiagram(start,
     document.getElementById('container'));
